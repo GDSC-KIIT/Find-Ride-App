e8ed353bb9e00ab1b78bdfbd4586ab0d91703eb0
chak_Kritip <1805216@kiit.ac.in>
2020-05-08 22:28:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+     private FusedLocationProviderClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button1);
+
+        requestPermission();
+        client= LocationServices.getFusedLocationProviderClient(this);
+
+
+
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(ActivityCompat.checkSelfPermission(MainActivity.this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            TextView textView=findViewById(R.id.textView);
+            textView.setText("You have Desabled the Location");
+            return;
+        }
+        client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+
+                    if(location!=null)
+                    {
+                        TextView textView=findViewById(R.id.textView);
+                        textView.setText(location.toString());
+                    }
+                }
+            });
+    }
+
+
+    private void requestPermission()
+    {
+        ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION}, 1);
+    }
+}
