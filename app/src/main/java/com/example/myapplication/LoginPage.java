e727dec4d718dadfa7c7d39d6c50f1b45447bcca
Arpit Mishra <arpitmishra4779@gmail.com>
2020-05-12 16:20:06 +0530

    Login page Design edits done

--- app/src/main/java/com/example/myapplication/LoginPage.java
+++ app/src/main/java/com/example/myapplication/LoginPage.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class LoginPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginchoice);
+        setContentView(R.layout.activity_userlogin);
 
     }
 }
