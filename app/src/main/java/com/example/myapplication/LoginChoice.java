e4a6303e1341d189c7370be88b2aba0e6374f55f
chak_kritip <45520181+Kritip123@users.noreply.github.com>
2020-08-19 19:03:29 +0530

    new commit

--- app/src/main/java/com/example/myapplication/LoginChoice.java
+++ app/src/main/java/com/example/myapplication/LoginChoice.java
@@ -5,21 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginChoice extends Activity {
+    FirebaseAuth firebaseAuth;
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_loginchoice);
+        firebaseAuth=FirebaseAuth.getInstance();
     }
+
     //Directs to the user login page
-    public void userloginIntent(View view){
+    public void userloginIntent(View view) {
         Intent intent = new Intent(this, userloginPage.class);
         startActivity(intent);
     }
 
     //directs to the driver login or register page
-    public void driverloginIntent(View view){
+    public void driverloginIntent(View view) {
         Intent intent = new Intent(this, driverloginPage.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
